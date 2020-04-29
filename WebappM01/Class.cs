46f138d22b741c0b02314ecde06b336e64301245
suman-TestRepo <suman.shee@globant.com>
2020-04-29 15:53:39 +0530

    helloworldtestapp

---
+++ WebappM01/Class.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebappM01
+{
+    public class Class
+    {
+
+        private static void test(string[] args)
+        {
+            /* my first program in C# */
+            Console.WriteLine("Hello World1");
+            Console.ReadKey();
+        }
+
+
+
+    }
+}
